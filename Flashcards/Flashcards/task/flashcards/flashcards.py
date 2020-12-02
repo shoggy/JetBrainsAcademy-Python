a0d28c5eac87fe8f096e31a2cdc47657b47b7fa5
Vladimir Nesmelov <wladimir.shoggy@gmail.com>
2020-12-02 12:59:05 +0300

    Stage 3

--- Flashcards/Flashcards/task/flashcards/flashcards.py
+++ Flashcards/Flashcards/task/flashcards/flashcards.py
@@ -1,8 +1,24 @@
 # Write your code here
 
-card = (input(), input())
-answer = input()
-if card[1] == answer:
-    print('Your answer is right!')
-else:
-    print('Your answer is wrong...')
+class Card:
+
+    def __init__(self, term, definition) -> None:
+        super().__init__()
+        self.term = term
+        self.definition = definition
+
+
+cards_list = []
+n = int(input('Input the number of cards:\n'))
+for i in range(1, n + 1):
+    card = Card(input(f'The term for card #{i}:\n'),
+                input(f'The definition for card #{i}:\n'))
+    cards_list.append(card)
+
+for card in cards_list:
+    print(f'Print the definition of "{card.term}":')
+    answer = input()
+    if card.definition == answer:
+        print('Correct!')
+    else:
+        print(f'Wrong. The right answer is "{card.definition}".')
