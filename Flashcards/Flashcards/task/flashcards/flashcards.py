4880ebe3830e766ef015d2928164bd1986e5ce63
Vladimir Nesmelov <wladimir.shoggy@gmail.com>
2020-12-11 21:09:03 +0300

    Stage 4

--- Flashcards/Flashcards/task/flashcards/flashcards.py
+++ Flashcards/Flashcards/task/flashcards/flashcards.py
@@ -11,14 +11,23 @@ class Card:
 cards_list = []
 n = int(input('Input the number of cards:\n'))
 for i in range(1, n + 1):
-    card = Card(input(f'The term for card #{i}:\n'),
-                input(f'The definition for card #{i}:\n'))
+    term = input(f'The term for card #{i}:\n')
+    while any(x for x in cards_list if x.term == term):
+        term = input(f'The term "{term}" already exists. Try again:\n')
+    definition = input(f'The definition for card #{i}:\n')
+    while any(x for x in cards_list if x.definition == definition):
+        definition = input(f'The definition "{definition}" already exists. Try again:\n')
+    card = Card(term, definition)
     cards_list.append(card)
 
+rev_dict = {x.definition: x.term for x in cards_list}
 for card in cards_list:
     print(f'Print the definition of "{card.term}":')
     answer = input()
     if card.definition == answer:
         print('Correct!')
+    elif answer in rev_dict:
+        print(f'Wrong. The right answer is "{card.definition}", ' +
+              f'but your definition is correct for "{rev_dict[answer]}".')
     else:
         print(f'Wrong. The right answer is "{card.definition}".')
