92dfa6b11e3ab3a423551e36290bebd7c0547944
Vladimir Nesmelov <wladimir.shoggy@gmail.com>
2021-03-01 20:51:48 +0300

    Stage 2

--- Web Scraper/Web Scraper/task/webscraper/scraper.py
+++ Web Scraper/Web Scraper/task/webscraper/scraper.py
@@ -1,13 +1,22 @@
+import json
+
 import requests
+from bs4 import BeautifulSoup as bs
 
 
 def work():
     user_url = input('Input the URL:\n')
-    r = requests.get(user_url)
-    if r.status_code == 200 and 'content' in r.json():
-        return r.json()['content']
-    else:
-        return 'Invalid quote resource!'
+    res = 'Invalid movie page!'
+    try:
+        r = requests.get(user_url)
+        if r.status_code == 200:
+            soup = bs(r.content, 'html.parser')
+            resp_title = json.loads("".join(soup.find('script', {'type': 'application/ld+json'}).contents))['name']
+            resp_description = soup.select_one('.summary_text').text.strip()
+            if resp_title and resp_description:
+                res = json.dumps({'title': resp_title, 'description': resp_description})
+    finally:
+        return res
 
 
 print(work())
