c9a35dc9a2e990a4c08b9ca53c64c78ba0b331b2
Vladimir Nesmelov <wladimir.shoggy@gmail.com>
2021-03-01 22:22:37 +0300

    Stage 4

--- Web Scraper/Web Scraper/task/webscraper/scraper.py
+++ Web Scraper/Web Scraper/task/webscraper/scraper.py
@@ -1,15 +1,33 @@
+from bs4 import BeautifulSoup as bs
 import requests
+import string
+from furl import furl
 
 
-def work():
-    user_url = input('Input the URL:\n')
+def save_page(user_url: str, file_name: str):
     r = requests.get(user_url)
     if r.status_code == 200:
-        with open('source.html', mode='wb') as fout:
-            fout.write(r.content)
-        return 'Content saved.'
-    else:
-        return f'The URL returned {r.status_code}!'
+        with open(file_name, mode='wb') as fout:
+            fout.write(bs(r.content, 'html.parser').select_one('.article__body').text.strip().encode('utf-8'))
 
 
-print(work())
+def work(user_url):
+    f = furl(user_url)
+    r = requests.get(user_url)
+    articles = []
+    if r.status_code == 200:
+        soup = bs(r.content, 'html.parser')
+        for art in soup.select('article'):
+            art_type = art.select_one('span[data-test="article.type"]')
+            if art_type and art_type.get_text(strip=True) == 'News':
+                link = art.select_one('a[data-track-action="view article"]')
+                fc = f.copy()
+                fc.path.set(link['href'])
+                link_href = str(fc)
+                link_name = link.text.translate(str.maketrans(' ', '_', string.punctuation)) + '.txt'
+                articles.append(link_name)
+                save_page(link_href, link_name)
+    print(f'Saved articles: {articles}')
+
+
+print(work('https://www.nature.com/nature/articles'))
