849b242bea699e3e7cc3535c23674bf47c440da4
Vladimir Nesmelov <vladimir_nesmelov@epam.com>
2022-09-29 16:06:09 +0300

    Stage 3

--- Honest Calculator/Honest Calculator/task/honest_calc.py
+++ Honest Calculator/Honest Calculator/task/honest_calc.py
@@ -3,6 +3,8 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
 
 
 def get_num(s: str):
@@ -22,12 +24,35 @@ def operate(a, b, op: str):
         raise ValueError("bad args")
 
 
+def ask(question: str) -> bool:
+    while True:
+        print(question)
+        answer = input()
+        if answer == 'y':
+            return True
+        elif answer == 'n':
+            return False
+
+
+def ask_to_store() -> bool:
+    return ask(msg_4)
+
+
+def ask_to_continue() -> bool:
+    return ask(msg_5)
+
+
+memory = 0
 while True:
     print(msg_0)
     calc = input()
     [x, oper, y] = calc.split(' ')
     x_num = 0
     y_num = 0
+    if x == 'M':
+        x = memory
+    if y == 'M':
+        y = memory
     try:
         x_num = get_num(x)
         y_num = get_num(y)
@@ -39,8 +64,13 @@ while True:
         continue
     try:
         res = operate(x_num, y_num, oper)
-        print(res)
-        break
     except ValueError:
         print(msg_3)
         continue
+    print(res)
+    if ask_to_store():
+        memory = res
+    if ask_to_continue():
+        continue
+    else:
+        break
