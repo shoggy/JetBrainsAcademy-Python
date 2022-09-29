f5c38d9522f9a57fdad75a1e2f69c8b506492fec
Vladimir Nesmelov <vladimir_nesmelov@epam.com>
2022-09-29 15:43:48 +0300

    Stage 2

--- Honest Calculator/Honest Calculator/task/honest_calc.py
+++ Honest Calculator/Honest Calculator/task/honest_calc.py
@@ -2,26 +2,45 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
 
 
 def get_num(s: str):
-    try:
-        return int(s)
-    except ValueError:
-        return float(s)
+    return float(s)
+
+
+def operate(a, b, op: str):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/' and b:
+        return a / b
+    else:
+        raise ValueError("bad args")
 
 
 while True:
     print(msg_0)
     calc = input()
     [x, oper, y] = calc.split(' ')
+    x_num = 0
+    y_num = 0
     try:
-        x = get_num(x)
-        y = get_num(y)
+        x_num = get_num(x)
+        y_num = get_num(y)
     except ValueError:
         print(msg_1)
         continue
     if oper not in {'+', '-', '*', '/'}:
         print(msg_2)
-    else:
+        continue
+    try:
+        res = operate(x_num, y_num, oper)
+        print(res)
         break
+    except ValueError:
+        print(msg_3)
+        continue
