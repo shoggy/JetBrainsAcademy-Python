c4d7cbea0faa3c234e3e9f1ccfb6b92e69cf043a
Vladimir Nesmelov <vladimir_nesmelov@epam.com>
2022-09-30 17:11:57 +0300

    Stage 2

--- Duplicate File Handler/Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/Duplicate File Handler/task/handler.py
@@ -1,6 +1,7 @@
 # write your code here
 import argparse
 import os
+from collections import defaultdict, OrderedDict
 
 
 def get_root_dir() -> str:
@@ -11,10 +12,32 @@ def get_root_dir() -> str:
     return root
 
 
-def walk_dirs(root: str):
+def get_file_format() -> str:
+    print("Enter file format:")
+    return input()
+
+
+def ask_is_descending_option() -> bool:
+    msg = "Size sorting options:\n1. Descending\n2. Ascending"
+    print(msg)
+    while True:
+        print("Enter a sorting option:")
+        choice = input()
+        if choice not in {'1', '2'}:
+            print("Wrong option")
+            continue
+        else:
+            return choice == '1'
+
+
+def walk_dirs(root: str, filter_ext: str):
+    files_map = defaultdict(list)
     for root, dirs, files in os.walk(root):
         for name in files:
-            print(os.path.join(root, name))
+            file = os.path.join(root, name)
+            if file.endswith(filter_ext):
+                files_map[os.path.getsize(file)].append(file)
+    return {k: v for k, v in files_map.items() if len(v) > 1}
 
 
 def main():
@@ -22,7 +45,11 @@ def main():
     if root is None:
         print("Directory is not specified")
         exit()
-    walk_dirs(root)
+    file_map = walk_dirs(root, get_file_format())
+    is_desc = ask_is_descending_option()
+    file_map = OrderedDict(sorted(file_map.items(), reverse=is_desc))
+    for k, v in file_map.items():
+        print(f"{k} bytes", *v, sep='\n')
 
 
 if __name__ == '__main__':
